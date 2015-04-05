918077bca7602165b6603838c1b53b364346f547
mamachanko <max@rootswiseyouths.com>
2015-04-05 22:06:23 +0200

    basic camp tools

--- camp.py
+++ camp.py
@@ -14,7 +14,9 @@ class Camp(object):
 
     WHEELDIR = 'wheelhouse'
 
-    def __init__(self, path):
+    def __init__(self, path=None):
+        if path is None:
+            path = os.getcwd()
         self.path = path
         self.packages = set()
         self.collect_packages() 
@@ -29,22 +31,23 @@ class Camp(object):
             else:
                 self.packages.add(package)
 
+    def create_wheels(self):
+        wheeldir = os.path.join(os.getcwd(), self.WHEELDIR)
+        wheel_command_template = 'pip wheel --no-cache-dir --wheel-dir={} {}'
+
+        for requirement in self.get_requirements():
+            wheel_command = wheel_command_template.format(wheeldir, requirement)
+            parsed_wheel_command = shlex.split(wheel_command)
+            wheel_command_process = Popen(parsed_wheel_command, stdout=PIPE, stderr=PIPE)
+            output, errors = wheel_command_process.communicate()
+
     def get_requirements(self):
         requirements = set()
         for package in self.packages:
             requirements.update(package.requirements)
         return requirements
-        
-    def create_wheels(self):
-        wheeldir = os.path.join(os.getcwd(), self.WHEELDIR)
-        wheel_command_template = 'pip wheel --no-cache-dir --wheel-dir={} {}'.format(wheeldir)
 
-        for requirement in self.get_requirements():
-            wheel_command = wheel_command_template.format(requirement)
-            wheel_command_process = Popen(wheel_command, stdout=PIPE, stderr=PIPE)
-            output, errors= command.communicate()
-
-    def remove_wheels(path):
+    def remove_wheels(self):
         shutil.rmtree(os.path.join(self.path, self.WHEELDIR), ignore_errors=True)
 
 
@@ -58,11 +61,12 @@ class Package(object):
 
     def validate(self):
         if not os.path.isdir(self.path):
-            raise NoPythonPackageError('{} must be directory'.format(path))
+            raise NoPythonPackageError('{} must be directory'.format(self.path))
         must_haves = ('setup.py', 'requirements.txt')
         if not all(path in os.listdir(self.path) for path in must_haves):
             raise NoPythonPackageError('cannot find setup.py or requirements.txt in {}'.format(self.path))
 
+    @property
     def requirements(self):
         try:
             return self._requirements
@@ -75,22 +79,26 @@ class Package(object):
             self._requirements = requirements
             return self._requirements
 
-    #FIXME
+    @property
+    def venv(self):
+        return os.path.join(self.path, self.VENV)
+
+    @property
+    def requirements_file(self):
+        return os.path.join(self.path, 'requirements.txt')
+
     def get_installed(self):
-        self.run_in_venv('pip freeze')
+        return self.run_in_venv('pip freeze')[0].split('\n')
         
     def install_requirements(self):
         wheeldir = os.path.join(os.getcwd(), 'wheelhouse')
-        install_command = 'pip install --use-wheel --no-index --find-links={} -r requirements.txt'.format(wheeldir)
-        self.run_in_venv(install_command)
+        install_command = 'pip install --use-wheel --no-index --find-links={} -r {}'.format(wheeldir, self.requirements_file)
+        return self.run_in_venv(install_command)
 
     def create_venv(self, force=False):
         if self.has_venv() and not force:
             return
-        with working_directory(path):
-            command = Popen(['virtualenv', self.VENV], stdout=PIPE)
-            output = command.communicate()[0]
-            output = output.rstrip('\n')
+        return self.run_in_path('virtualenv {}'.format(self.VENV))
 
     def has_venv(self):
         return self.VENV in os.listdir(self.path)
@@ -98,29 +106,20 @@ class Package(object):
     def remove_venv(self):
         shutil.rmtree(os.path.join(self.path, self.VENV), ignore_errors=True)
 
-    def run_in_path(self, commands):
-        with working_directory(self.path):
-            for command in commands:
-                parsed_command = shlex.split(command)
-                command_process = Popen(parsed_command, stdout=PIPE, stderr=PIPE)
-                output, errors = command_process.communicate()
-                if errors:
-                    click.echo(errors)
-                else:
-                    click.echo(output)
+    def run_in_venv(self, command):
+        command_template = '/bin/bash -c "source {}/bin/activate && {}"'
+        command = command_template.format(self.venv, command)
+        return self.run_in_path(command, path=self.venv)
 
-    def run_in_venv(self, commands):
-        for command in commands:
-            command_template = '/bin/bash -c "source {}/venv/bin/activate && {}"'
-            command = command_template.format(command)
+    def run_in_path(self, command, path=None):
+        if path is None:
+            path = self.path
+
+        with working_directory(path):
             parsed_command = shlex.split(command)
-            command_process = Popen(command, stdout=PIPE, stderr=PIPE, shell=False)
-            output, errors = command_process.communicate()
-            if errors:
-                click.echo(errors)
-            else:
-                click.echo(output)
-        
+            command_process = Popen(parsed_command, stdout=PIPE, stderr=PIPE, shell=False)
+            return command_process.communicate()
+
 
 class working_directory(object):
 
@@ -135,130 +134,58 @@ class working_directory(object):
         os.chdir(self.old_working_directory)
 
 
-def find_packages(path):
-    packages = set()
-    for entry in os.listdir(path):
-        absolute_path = os.path.abspath(entry)
-        if os.path.isfile(absolute_path):
-            continue
-        must_haves = ('setup.py', 'requirements.txt')
-        if all(path in os.listdir(absolute_path) for path in must_haves):
-            packages.add(absolute_path)
-    return packages
-
-
-def create_venv(path, overwrite=False):
-    if 'venv' in os.listdir(path) and not overwrite:
-        return
-    with working_directory(path):
-        command = Popen(['virtualenv', 'venv'], stdout=PIPE)
-        output = command.communicate()[0]
-        output = output.rstrip('\n')
-
-
-def remove_venv(path):
-    shutil.rmtree(os.path.join(path, 'venv'), ignore_errors=True)
-
-
-def get_requirements(packages):
-    requirements = set()
-    for package in packages:
-        with open(os.path.join(package, 'requirements.txt')) as requirements_file:
-            reqs = requirements_file.readlines()
-            reqs = map(lambda path: path.rstrip('\n'), reqs)
-            requirements.update(reqs)
-    return requirements
-
-
-def create_wheels(packages, wheeldir=None):
-    if wheeldir is None:
-        wheeldir = os.path.join(os.getcwd(), 'wheelhouse')
-    
-    wheel_command = ['pip', 'wheel', '--no-cache-dir', '--wheel-dir=%s' % wheeldir]
-
-    for package in packages:
-        command = Popen(wheel_command + [package], stdout=PIPE)
-        output = command.communicate()[0]
-        output = output.rstrip('\n')
-
-
-def remove_wheels(path):
-    shutil.rmtree(os.path.join(path, 'wheelhouse'), ignore_errors=True)
-
-
-def run_in_venv(venv, command):
-    with working_directory(venv):
-        command_template = '/bin/bash -c "source {}/venv/bin/activate && {}"'
-        command = shlex.split(command_template.format(venv, command))
-        process = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=False)
-    return process.communicate()
-
-
-def show_packages(package):
-    pip = os.path.join(package, 'venv/bin/pip')
-    output = run_in_venv(package, 'pip freeze')[0]
-    click.echo('packages in {}:\n{}'.format(package, output))
-
-
-def install_requirements(package, wheeldir=None):
-    if wheeldir is None:
-        wheeldir = os.path.join(os.getcwd(), 'wheelhouse')
-    install_command = 'pip install --use-wheel --no-index --find-links={} -r requirements.txt'.format(wheeldir)
-    run_in_venv(package, install_command)
+pass_camp = click.make_pass_decorator(Camp, ensure=True)
 
 
 @click.group()
-def camp():
+@pass_camp
+def cli(camp):
     click.echo('this is camp\n')
 
 
-@camp.command()
-def up():
-    cwd = os.getcwd()
-    packages = find_packages(cwd)
-    click.echo('found Python repositories')
+@cli.command()
+@pass_camp
+def up(camp):
+    click.echo('found Python packages')
     click.echo()
-    for package in packages:
-        click.echo('    {}'.format(package))
+    for package in camp.packages:
+        click.echo('    {}'.format(package.path))
     click.echo()
 
     click.echo('creating virtualenvs...')
-    map(create_venv, packages)
+    for package in camp.packages:
+        package.create_venv()
     click.echo('done\n')
 
     click.echo('getting wheels...')
-    requirements = get_requirements(packages) 
-    create_wheels(requirements)
+    camp.create_wheels()
     click.echo('done\n')
 
     click.echo('installing requirements...')
-    map(install_requirements, packages)
+    for package in camp.packages:
+        package.install_requirements()
     click.echo('done')
 
 
-@camp.command()
-def down():
-    cwd = os.getcwd()
-    packages = find_packages(cwd)
-
+@cli.command()
+@pass_camp
+def down(camp):
     click.echo('removing virtualenvs...')
-    map(remove_venv, packages)
+    for package in camp.packages:
+        package.remove_venv()
     click.echo('done\n')
 
     click.echo('removing wheels...')
-    remove_wheels(cwd)
+    camp.remove_wheels()
     click.echo('done')
 
 
-@camp.command()
-def show():
-    cwd = os.getcwd()
-    packages = find_packages(cwd)
-
-    click.echo('found Python repositories')
+@cli.command()
+@pass_camp
+def show(camp):
+    click.echo('found Python packages')
     click.echo()
-    for package in packages:
-        click.echo('    {}'.format(package))
-    click.echo()
-
-    map(show_packages, packages)
+    for package in camp.packages:
+        click.echo('    {}'.format(package.path))
+        for installed in package.get_installed():
+            click.echo('        {}'.format(installed))
